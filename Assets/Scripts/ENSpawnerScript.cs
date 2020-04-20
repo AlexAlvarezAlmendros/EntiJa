fc4ed2eaa61ec64fb183faa6c87995c525cccf0d
Enti/Carlos Sierra <carlos.sierra@enti.cat>
2020-04-20 13:44:04 +0200

    enemy1 basico

---
+++ Assets/Scripts/ENSpawnerScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ENSpawnerScript : MonoBehaviour
+{
+    public GameObject Enemy1Prefab;
+    private Enemy1Script enemy1Script;
+
+    public GameObject Camara;
+    private CameraScript cameraScript;
+
+    public float maxDelay;
+    public float minDelay;
+
+    public float FirstSpawn = 0f;
+    private float nextSpawn = 0f;
+
+    public int whatToSpawn;
+    public float whereToSpawn;
+    public float whenToSpawn;
+
+    void Start()
+    {
+        nextSpawn = FirstSpawn;
+    }
+
+    void Update()
+    {
+        if (Time.time > nextSpawn) // if time has come
+        {
+            whatToSpawn = Random.Range(1, 5); //define random value between 1 and 4 (5 is ecluded)
+
+            whereToSpawn = Random.Range(0, 5); // define random value between 0 and 4 (5 is excluded)
+
+            whenToSpawn = Random.Range(minDelay, maxDelay); // define random time value to spawn
+
+            if (whatToSpawn == 1)
+            {
+                GameObject enemySpawned = Instantiate(Enemy1Prefab, new Vector3(-5.5f, whereToSpawn, 1f), Quaternion.identity);
+                enemy1Script = enemySpawned.GetComponent<Enemy1Script>();
+                //enemyScript.powerUpType = powerUpType.Energy;
+            }
+            //else if (whatToSpawn == 2)
+            //{
+            //    GameObject enemySpawned = Instantiate(Enemy2Prefab, new Vector3(-5.5f, whereToSpawn, 1f), Quaternion.identity);
+            //    powerUpScript = enemySpawned.GetComponent<PowerUpScript>();
+            //    powerUpScript.powerUpType = powerUpType.Boost;
+            //}
+            //else if (whatToSpawn == 3)
+            //{
+            //    GameObject enemySpawned = Instantiate(PowerUpPrefab, new Vector3(-5.5f, whereToSpawn, 1f), Quaternion.identity);
+            //    powerUpScript = enemySpawned.GetComponent<PowerUpScript>();
+            //    powerUpScript.powerUpType = powerUpType.Shield;
+            //}
+            //else if (whatToSpawn == 4)
+            //{
+            //    GameObject enemySpawned = Instantiate(PowerUpPrefab, new Vector3(-5.5f, whereToSpawn, 1f), Quaternion.identity);
+            //    powerUpScript = enemySpawned.GetComponent<PowerUpScript>();
+            //    //powerUpScript.powerUpType = powerUpType.PW4;
+            //}
+
+            nextSpawn = Time.time + whenToSpawn;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy1Script.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1Script : MonoBehaviour
+{
+    public int EnemyHP = 1;
+    public float jumpStrengh = 100;
+    public float jumpDelay = 1000;
+    private float jumpTmp = 0;
+    private bool jumped = true;
+    private int JumpedID;
+
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+    private SpriteRenderer renderer;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        renderer = GetComponent<SpriteRenderer>();
+        JumpedID = Animator.StringToHash("Jumped");
+        enabled = false;
+    }
+
+    void FixedUpdate()
+    {
+        float delta = Time.deltaTime * 1000;
+
+        //if (EnemyHP == 0)
+        //{
+        //    GameManager.Instance.score += 100;
+        //    cameraScript = CameraController.GetComponent<CameraControl>();
+        //    cameraScript.auidoS.clip = SoundManager.Instance.Enemy2Death;
+        //    cameraScript.auidoS.Play();
+
+        //    //Destroy(gameObject); //DESTROY
+        //}
+
+        if (jumped == true && jumpTmp < jumpDelay) { jumpTmp += delta; }
+        else if (jumpTmp >= jumpDelay) { jumped = false; jumpTmp = 0; }
+        
+        if (jumped == false)
+        {
+            rigidBody.AddForce(Vector2.right * -jumpStrengh * delta, ForceMode2D.Impulse);
+            rigidBody.AddForce(Vector2.up * -jumpStrengh * delta, ForceMode2D.Impulse);
+            jumped = true;
+        }
+    }
+}
