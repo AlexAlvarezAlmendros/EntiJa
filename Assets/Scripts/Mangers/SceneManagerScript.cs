7f9215cbaf3e06154f47118ccefb1fe4cea10bd7
AlexAlvarezAlmendros <alexalmendrosal@gmail.com>
2020-04-21 20:06:57 +0200

    Musicaaaaaaa

--- Assets/Scripts/Mangers/SceneManagerScript.cs
+++ Assets/Scripts/Mangers/SceneManagerScript.cs
@@ -6,7 +6,14 @@ using UnityEngine.SceneManagement;
 public class SceneManagerScript : MonoBehaviour
 {
     public void ChangeScene (string scName) {
-        
+        switch (scName)
+        {
+            case "AlexScene":
+                FindObjectOfType<AudioManager>().Stop("MenuMusic");
+                break;
+            default:
+                break;
+        }
         SceneManager.LoadScene(scName);
     }
 
