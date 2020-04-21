f85ef18d48c5d5b347f1fd385a8564c1f606845b
Enti/Carlos Sierra <carlos.sierra@enti.cat>
2020-04-21 14:43:39 +0200

    sets

--- Assets/Scripts/Mangers/MusicManager.cs
+++ Assets/Scripts/Mangers/MusicManager.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class MusicManager : Singleton<MusicManager>
 {
     public AudioClip[] musicList;
+    private AudioClip currentClip;
 
     AudioSource audioSource;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic(int numA)
+    {
+        currentClip = musicList[numA];
+
+        audioSource.clip = currentClip;
+        audioSource.Play(0);
+    }
+
+    public void PauseMusic(int numA)
+    {
+        audioSource.Pause();
     }
 }
