227b62bf5f1fbd2ee6f1e4839ca27bb684946b77
Enti/Carlos Sierra <carlos.sierra@enti.cat>
2020-04-22 08:54:13 +0200

    Score in GameOver

---
+++ Assets/GameOverScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameOverScript : MonoBehaviour
+{
+    public Text text;
+
+    void Start()
+    {
+        text.text = GameController.instance.hiscore + "km";
+    }
+
+    void Update()
+    {
+        
+    }
+}
